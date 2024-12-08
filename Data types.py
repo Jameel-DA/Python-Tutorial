fb19d796ba415ecc9013887572fcd3b49f62a652
Jameel Ahmed <jameelahmed.tech@gmail.com>
2024-12-08 19:16:36 +0530

    Create Data types.py

---
+++ Data types.py
@@ -0,0 +1,14 @@
+""" Data type is a Type of Data it is use for declaring a variable.
+
+    Python Data types are the classification or categorization of data items. It represents the kind of value that tells what operations can be performed on a particular data.
+
+    The following are the standard or built-in data types in Python:
+
+    Text-type: String
+    Numeric Types: Integer(int), Floating point(float), Complex
+    Sequence Types: list, tuple and range
+    Boolean Type: bool
+    Set Type: set, frozenset
+    Mapping Type: dictionaries(dict)
+    Binary Types: bytes, bytearray, memoryview
+"""
